5ffb8f7f6b418990b1debe63f3725b95ffd104f7
Wilder MO <66009198+WilderMO@users.noreply.github.com>
2021-10-26 18:28:44 +0200

    Add files via upload

---
+++ MO-SeatShuffle/__resource.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Wilder MO'
+description 'Vehicle Seat Shuffle for JAMA-RP by https://github.com/WilderMO/'
+version '1.0.0'
+
+client_script 'client.lua'
